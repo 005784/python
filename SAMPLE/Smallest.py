6912b1db54708bbb32f467245ddd026405e7e0a5
005784 <Mallela.pranitha@jktech.com>
2022-02-15 13:16:24 +0530

    MY FIRST GIT FILE

---
+++ SAMPLE/Smallest.py
@@ -0,0 +1,9 @@
+a=(input("enter a:"))
+b=(input("enter b:"))
+c=(input("enter c:"))
+if a<b and a<c:
+    print(a,"is smallest")
+elif b<a and b<c:
+    print(b,"is smallest")
+else :
+    print(c,"is smallest")

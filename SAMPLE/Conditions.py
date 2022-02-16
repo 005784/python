cdd05727cc733ea3a5539ba378e7e8b9972f6881
005784 <Mallela.pranitha@jktech.com>
2022-02-16 11:02:03 +0530

    my first git

---
+++ SAMPLE/Conditions.py
@@ -0,0 +1,10 @@
+num=-1
+# if(num>0):
+#     print(num,"is greater")
+# print("welcome to python")#if statement
+num1=2
+# if(num>num1):
+#     print(num ,"is greater")
+# else:
+#     print(num1,"is greater")#if else statement
+
